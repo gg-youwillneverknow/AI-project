1ba9103632ad42a10b4ba6c23ac786de0b2dc117
Ginevra Cepparulo <ginevra.cepparulo@icloud.com>
2022-05-05 00:02:35 +0200

    running program

--- input_parser.py
+++ input_parser.py
@@ -1,33 +1,11 @@
+import enum
 from pickle import FALSE
 import re, sys
 
 pattern = r'((?:(?:High|Low);){3}[ENW]);((?:(?:High|Low);){2}(?:High|Low))'
+dir = "N"
 
-def main(filename):
-	f = open(filename, "r")
-	transition_table = transition_table(f)
-	states = []
-	for a in ["High", "Low"]:
-		for b in ["High", "Low"]:
-			for c in ["High", "Low"]:
-				states += a+";"+b+";"+c
-	expected_costs = {}
-	policies = {}
-	flag=True
-	while(flag):
-		flag = False
-		for state in states:
-			policy,min = value_iteration(state,transition_table,expected_costs)
-			if expected_costs[state]!=min:
-				expected_costs[state]=min
-				policies[state]=policy
-				flag = True
-	print(policies)
-	print(expected_costs)
-	pass
-
-			
-def transition_table(f):
+def make_transition_table(f):
 	
 	dict={}
 	#do some re and print the output, let's say 5 times
@@ -49,9 +27,9 @@ def transition_table(f):
 			elif m.group(2) not in dict[m.group(1)]:
 				dict[m.group(1)][m.group(2)]=1
 			
-	print("the number of initial states: ",len(dict))
+	#print("the number of initial states: ",len(dict))
 	l = [len(key) for key in dict]
-	print(l)
+	#print(l)
 	reachable = []
 	for key in dict:
 		for second_key in dict[key]:
@@ -59,9 +37,9 @@ def transition_table(f):
 				if second_key+d not in dict:
 					non_initial += [second_key+d]
 			reachable+=[second_key]
-	print(set(reachable))
-	print(set(non_initial))
-	print(dict)
+	#print(set(reachable))
+	#print(set(non_initial))
+	#print(dict)
 	
 	for state in dict: 
 
@@ -70,10 +48,39 @@ def transition_table(f):
 			dict[state][final_state] = dict[state][final_state] / total
 	return dict
 
+def main(filename):
+	f = open(filename, "r")
+	transition_table = make_transition_table(f)
+	states = []
+	for i,a in enumerate(["High", "Low"]):
+		for j,b in enumerate(["High", "Low"]):
+			for k,c in enumerate(["High", "Low"]):
+				state= a+";"+b+";"+c
+				states+=[state]
+	print('states are',states)
+	expected_costs = {}
+	policies = {}
+	flag=True
+	while(flag):
+		flag = False
+		for state in states:
+			policy,min = value_iteration(state,transition_table,expected_costs)
+			if expected_costs.get(state,0)!=min:
+				expected_costs[state]=min
+				policies[state]=policy
+				flag = True
+	print(policies)
+	print("")
+	print(expected_costs)
+	pass		
+
+
 def value_iteration(state,transition_table,expected_costs):
 	min = False
 	policy = False
-	for action in actions(state):
+	for i,action in enumerate(actions(state)):
+		if state == "Low;Low;Low":
+			return action,0
 		value = cost_function(state,action)
 		for final_state in transition_table[state+';'+action]:
 			value += transition_table[state+';'+action][final_state]*expected_costs.get(final_state,0)
@@ -84,7 +91,20 @@ def value_iteration(state,transition_table,expected_costs):
 	return policy,min
 
 def actions(state):
-	return actions
+	if state == "Low;Low;Low":
+		return next_loop()
+	else:
+		return ["N","E","W"]
+
+def next_loop():
+	global dir
+	if dir=="N":
+		dir="E"
+	elif dir=="E":
+		dir="W"
+	else:
+		dir="N"
+	return [dir]
 
 def cost_function(state,action):
 	return 1
